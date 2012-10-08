a0404042560c2af68a3d6fdf2455a56115f2debe
Bobik <pushax@ukr.net>
2012-10-08 15:43:50 +0300

    vbn

--- src/main/java/epam/ph/sg/controllers/HomeController.java
+++ src/main/java/epam/ph/sg/controllers/HomeController.java
@@ -12,16 +12,16 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 import epam.ph.sg.models.LangModelDAO;
 
 @Controller
-
+@SessionAttributes("lang")
 public class HomeController {
-
+	
 	@RequestMapping("/index.html")
 	public String index(HttpSession session) {
 		ApplicationContext ctx = new ClassPathXmlApplicationContext(
 				"springconfig.xml");
 		LangModelDAO lang = (LangModelDAO)ctx.getBean("defaultLang");
 		//Мова аплікухи в сесії
-		session.setAttribute("lang", lang);
+		session.setAttribute("lang",lang);
 		if (session.getAttribute("user") == null) {
 			return "Login";
 		} else {
