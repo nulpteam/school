5d5f749fdac31ce42c24ec7ef2d1bca2afddafd7
monzaroman <romanmakovyak@gmail.com>
2012-12-12 16:29:46 +0200

    рандомна генерація поля .два пінгвіна

--- src/main/java/epam/ph/sg/tab/twopenguins/TwoPenguinsFieldCreator.java
+++ src/main/java/epam/ph/sg/tab/twopenguins/TwoPenguinsFieldCreator.java
@@ -12,22 +12,37 @@ import java.util.Random;
 public class TwoPenguinsFieldCreator {
 	public static int[][] generateField() {
 
-		
 		int[][] field = new int[3][4];
-		
-		field[0][0] = 2;
-		field[0][1] = 6;
-		field[0][2] = 5;
-		field[1][0] = 5;
-		field[1][1] = 6;
-		field[1][2] = 4;
-		field[2][0] = 4;
-		field[2][1] = 2;
-		field[2][2] = 7;
-		field[0][3] = 3;
-		field[1][3] = 7;
-		field[2][3] = 3;
-		
+
+		field[0][0] = 0;
+		field[0][1] = 0;
+		field[0][2] = 0;
+		field[1][0] = 0;
+		field[1][1] = 0;
+		field[1][2] = 0;
+		field[2][0] = 0;
+		field[2][1] = 0;
+		field[2][2] = 0;
+		field[0][3] = 0;
+		field[1][3] = 0;
+		field[2][3] = 0;
+
+		Random generator = new Random();
+
+		int a = 0;
+		int b = 0;
+		// 2
+
+		for (int i = 2; i < 8; i++) {
+			for (int j = 0; j < 2; j++) {
+				while (field[a][b] != 0) {
+					a = generator.nextInt(3);
+					b = generator.nextInt(4);
+				}
+				field[a][b] = i;
+			}
+		}
+
 		return field;
-	}	
+	}
 }
